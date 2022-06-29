5131e19e02ce9eb845bede9541e650d3d66ff413
Sơn Nguyễn <61671615+sonbill@users.noreply.github.com>
2022-06-29 11:56:04 +0700

    setup font, tailwind, composition nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-
+  <div>
+    <p class="font-bold">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto
+      aliquid similique repellat quisquam amet sit, unde possimus quod itaque
+      deleniti! Tenetur ipsum porro illo ratione nam temporibus fuga, quam
+      dolores quis atque cupiditate doloremque itaque dolor blanditiis
+      voluptatibus? Eum maxime reiciendis nam laborum error quam laboriosam
+      inventore necessitatibus eaque impedit.
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
+
+<style  >
+p {
+  font-family: "nunito";
+}
+</style>
