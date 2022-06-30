eeb792dd81932e3324dd6f08befc00244f240f1d
Sơn Nguyễn <61671615+sonbill@users.noreply.github.com>
2022-06-30 16:20:02 +0700

    create new header

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>
-    <div>Logo</div>
-    <div>
-      <Nuxt-link to="/about">About</Nuxt-link>
-      <Nuxt-link to="/contact">Contact</Nuxt-link>
-      <Nuxt-link to="/shop">Shop</Nuxt-link>
+  <div class="max-w-[626px] mx-auto">
+    <!-- LOGO -->
+    <div class="flex items-center justify-center">
+      <div class="w-[43px] h-[43px]">
+        <img
+          src="~/assets/img/logo-none-bg.png"
+          alt="logo-deverhub-blog"
+          class="w-full h-full object-cover"
+        />
+      </div>
+      <h1>DEVERHUB <span>BLOG</span></h1>
+    </div>
+    <!-- NAVIGATOR -->
+    <div class="flex justify-between items-center text-[20px] font-bold">
+      <Nuxt-link to="/home">Home</Nuxt-link>
+      <Nuxt-link to="/contact">Technology</Nuxt-link>
+      <Nuxt-link to="/programming">Programming</Nuxt-link>
+      <Nuxt-link to="/tricks">Tricks</Nuxt-link>
     </div>
   </div>
 </template>
--- layouts/loginAndRegister.vue
+++ layouts/loginAndRegister.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="h-screen bg-[#F5F5F5]">
-    <Header />
+  <div class="h-screen">
+    <!-- <Header /> -->
     <div class="grid grid-rows-1 grid-cols-1">
       <div class="w-[626px] h-full mx-auto">
         <Nuxt />
