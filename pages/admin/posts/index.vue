3fb31c7802c73fdcb90f3d3bb190ce597d12801e
Sơn Nguyễn <61671615+sonbill@users.noreply.github.com>
2022-08-09 20:03:00 +0700

    create new admin/posts page

---
+++ pages/admin/posts/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full p-5">
+    <div>
+      <h1>POSTS</h1>
+    </div>
+    <Table />
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "admin",
+};
+</script>
+
