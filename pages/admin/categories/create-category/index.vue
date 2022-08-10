f7abdbd33af3370eee2bb0b4cb98e22be6066a1d
Sơn Nguyễn <61671615+sonbill@users.noreply.github.com>
2022-08-10 21:02:02 +0700

    create new admin/user & create category func

---
+++ pages/admin/categories/create-category/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-col justify-center items-center min-h-screen h-full">
+    <div class="mb-3">
+      <h1 class="font-bold text-[30px]">CREATE CATEGORY</h1>
+    </div>
+    <form class="flex flex-col mb-3">
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Enter your category"
+        class="p-3 border mb-5 w-[300px]"
+      />
+      <button
+        type="submit"
+        class="border py-2 px-3 hover:bg-black hover:text-white"
+      >
+        Submit
+      </button>
+    </form>
+    <Nuxt-link to="#" class="hover:underline">Back</Nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "admin",
+};
+</script>
+
--- pages/admin/users/index.vue
+++ pages/admin/users/index.vue
@@ -1,30 +1,27 @@
 <template>
   <div class="w-full p-5">
     <div class="my-10">
-      <h1 class="font-bold text-[30px]">POSTS</h1>
-    </div>
-    <div>
-      <button class="bg-black hover:bg-gray-900 px-3 py-2 text-white mb-5">
-        CREATE CATEGORY
-      </button>
+      <h1 class="font-bold text-[30px]">USERS</h1>
     </div>
     <table class="table-auto w-full p-5">
       <thead>
         <tr class="w-full border-b-[1px]">
           <th>ID</th>
-          <th>CATEGORY</th>
+          <th>USERNAME</th>
+          <th>ROLE</th>
+          <th>DATE</th>
           <th></th>
         </tr>
       </thead>
       <tbody>
         <tr class="text-center">
           <td class="p-5">1</td>
-          <td class="p-5">Web Developer</td>
+          <td class="p-5">sonpewds</td>
+          <td class="p-5">Admin</td>
+          <td class="p-5">10-08-2022 - 20:13:14</td>
           <td>
             <div class="flex items-center space-x-3">
-              <button class="flex items-center justify-center">
-                <span class="material-icons"> mode_edit </span>
-              </button>
+              <button>DETAIL</button>
               <button class="flex items-center justify-center">
                 <span class="material-icons"> clear </span>
               </button>
@@ -36,11 +33,9 @@
   </div>
 </template>
 
-
 <script>
 export default {
   layout: "admin",
-  setup() {},
 };
 </script>
 
